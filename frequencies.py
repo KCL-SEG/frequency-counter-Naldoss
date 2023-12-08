8fbb29511f8849d86c1fffabe27571645e219adc
Nouf Aldossary <nouf.aldossary@kcl.ac.uk>
2023-12-08 02:28:06 +0000

    passed all tests

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in items:
+        key = str(i)
+        if (key in frequencies):
+            frequencies[str(i)] += 1
+        else:
+            frequencies[str(i)] = 1
+
     return frequencies
